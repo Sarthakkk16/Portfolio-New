3652a2cda8ba094e3f68189bd87af5f64b734e51
Sarthak Sharma <142617822+Sarthakkk16@users.noreply.github.com>
2026-05-19 22:52:43 +0530

    Updates

--- assets/Navbar-BuC3l0mJ.js
+++ assets/Navbar-BuC3l0mJ.js
@@ -1,4 +1,4 @@
-import{j,r as bt}from"./index-NiksMozW.js";import{g as St,S as yt}from"./ScrollTrigger-0lJgtTtq.js";/*!
+import{j,r as bt}from"./index-DRtFhi1S.js";import{g as St,S as yt}from"./ScrollTrigger-0lJgtTtq.js";/*!
  * SplitText 3.14.2
  * https://gsap.com
  *
