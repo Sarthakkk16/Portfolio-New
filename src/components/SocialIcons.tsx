a6296aeaface365363a4857c08db97ac12e91ef5
Sarthak Sharma <142617822+Sarthakkk16@users.noreply.github.com>
2026-05-19 23:10:59 +0530

    Update resume

--- src/components/SocialIcons.tsx
+++ src/components/SocialIcons.tsx
@@ -98,7 +98,7 @@ const SocialIcons = () => {
       </div>
       <a
         className="resume-button"
-        href="/Portfolio-New/resume1.pdf"
+        href={`${import.meta.env.BASE_URL}resume1.pdf`}
         target="_blank"
         rel="noreferrer"
       >
