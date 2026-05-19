8857c8caca314646240508c7855775caf585564b
Sarthak Sharma <142617822+Sarthakkk16@users.noreply.github.com>
2026-05-19 21:58:32 +0530

    Updates

--- assets/Navbar-6UB80ieZ.js
+++ assets/Navbar-6UB80ieZ.js
@@ -1,4 +1,4 @@
-import{j,r as bt}from"./index-B3ckNXAy.js";import{g as St,S as yt}from"./ScrollTrigger-0lJgtTtq.js";/*!
+import{j,r as bt}from"./index-CZ_Ax8-q.js";import{g as St,S as yt}from"./ScrollTrigger-0lJgtTtq.js";/*!
  * SplitText 3.14.2
  * https://gsap.com
  *
