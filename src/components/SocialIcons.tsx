d679dfc4cc852d5d58555c3630d64e85d9de5ea5
Sarthak Sharma <142617822+Sarthakkk16@users.noreply.github.com>
2026-05-19 23:01:27 +0530

    Fixed resume

--- src/components/SocialIcons.tsx
+++ src/components/SocialIcons.tsx
@@ -98,7 +98,7 @@ const SocialIcons = () => {
       </div>
       <a
         className="resume-button"
-        href="/Portfolio-New/Sarthak-Resume.pdf"
+        href="/Portfolio-New/resume1.pdf"
         target="_blank"
         rel="noreferrer"
       >
